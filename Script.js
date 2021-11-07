0ad57b883528719d8f874b79cd28677ced5bb8c8
Gabriel Vitorino <gabrielstosvitorino@gmail.com>
2021-11-07 18:37:33 -0300

    Só bora

--- Script.js
+++ Script.js
@@ -15,3 +15,36 @@ function reveal(){
     }
 }
 
+window.addEventListener('scroll', reveal2);
+function reveal2(){
+    var reveals = document.querySelectorAll('.reveal2');
+    for(var i = 0; i < reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+window.addEventListener('scroll', reveal3);
+function reveal3(){
+    var reveals = document.querySelectorAll('.reveal3');
+    for(var i = 0; i < reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
