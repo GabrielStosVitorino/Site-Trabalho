6b8496f38713066918c56558c303ff1aa2dba945
antoniorslima <antonio.lima@bandtec.com.br>
2021-11-26 15:05:44 -0300

    Só vai

--- public/Script.js
+++ public/Script.js
@@ -1,85 +1,67 @@
 window.addEventListener('scroll', reveal);
-function reveal(){
+function reveal() {
     var reveals = document.querySelectorAll('.reveal');
-    for(var i = 0; i < reveals.length; i++){
+    for (var i = 0; i < reveals.length; i++) {
         var windowheight = window.innerHeight;
         var revealtop = reveals[i].getBoundingClientRect().top;
         var revealpoint = 150;
 
-        if(revealtop < windowheight - revealpoint){
+        if (revealtop < windowheight - revealpoint) {
             reveals[i].classList.add('active');
         }
-        else{
+        else {
             reveals[i].classList.remove('active');
         }
     }
 }
 
 window.addEventListener('scroll', reveal2);
-function reveal2(){
+function reveal2() {
     var reveals = document.querySelectorAll('.reveal2');
-    for(var i = 0; i < reveals.length; i++){
+    for (var i = 0; i < reveals.length; i++) {
         var windowheight = window.innerHeight;
         var revealtop = reveals[i].getBoundingClientRect().top;
         var revealpoint = 150;
 
-        if(revealtop < windowheight - revealpoint){
+        if (revealtop < windowheight - revealpoint) {
             reveals[i].classList.add('active');
         }
-        else{
+        else {
             reveals[i].classList.remove('active');
         }
     }
 }
 
 window.addEventListener('scroll', reveal3);
-function reveal3(){
+function reveal3() {
     var reveals = document.querySelectorAll('.reveal3');
-    for(var i = 0; i < reveals.length; i++){
+    for (var i = 0; i < reveals.length; i++) {
         var windowheight = window.innerHeight;
         var revealtop = reveals[i].getBoundingClientRect().top;
         var revealpoint = 150;
 
-        if(revealtop < windowheight - revealpoint){
+        if (revealtop < windowheight - revealpoint) {
             reveals[i].classList.add('active');
         }
-        else{
+        else {
             reveals[i].classList.remove('active');
         }
     }
 }
 
-function enviaremail() {
-    var vemail = email.value;
-    var vas = assunto.value;
-    var vnome = nome.value;
-    var vmsg = mensagem.value;
-    
-    if (vnome.length == 0) {
-        alert('Nome incorreto');
-    }
+function tudo() {
+    fetch("/usuarios/listar", {
+        method: "GET",
+    }).then(function (resposta) {
 
-    if (vas.length < 10) {
-        alert('Defina um assunto maior');
-    }
+        resposta.json().then(function (data) {
+            let len = data.length;
 
-    if (vmsg.length < 15) {
-        alert('Mensagem muito pequena!');
-    }
-
-    if (vemail.indexOf('@') == -1 || vemail.length < 8 || vemail.indexOf('.') == -1) {
-        alert('Email Inválido!');
-    }
-
-    if (nome.value.length > 0){
-        if (assunto.value.length >= 10) {
-            if (mensagem.value.length >= 15) {
-                if (vemail.indexOf('@') != -1 && vemail.length >= 8 && vemail.indexOf('.') != -1) {
-                    alert("Sua mensagem foi enviada com sucesso! A Wegrowffee agradece o contato!");
-                    window.location.href = '#home';
-                }
-            }
-        }
-    }
-   
+            metrica.innerHTML = len;
+        });
+    }).catch(function (error) {
+        console.error(`ERROR`, error);
+    });
 }
+
+tudo();
